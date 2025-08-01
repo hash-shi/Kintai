458de66f13e7ea821d33db4cddd86a7c9a6b7d03
hash-kai <hash-kai@tjs-net.co.jp>
2025-08-01 17:10:16 +0900

    SrhMstKubunAction.java作成

---
+++ WEB-INF/src/jp/co/kintai/carreservation/action/dialog/SrhMstKubunAction.java
@@ -0,0 +1,51 @@
+package jp.co.kintai.carreservation.action.dialog;
+
+import java.sql.Connection;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import jp.co.kintai.carreservation.base.PJActionBase;
+import jp.co.tjs_net.java.framework.information.IndexInformation;
+
+public class SrhMstKubunAction extends PJActionBase {
+	public SrhMstKubunAction(HttpServletRequest req, HttpServletResponse res, IndexInformation info) {
+		super(req, res, info);
+	}
+	
+	@Override
+	public void doRun(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		
+		// 画面表示
+		this.setView("success");		
+		
+    }
+	
+	/**
+	 * 区分取得
+	 * 
+	 * @param req
+	 * @param res
+	 * @throws Exception
+	 */
+	public void search(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		
+		// 検索条件取得
+		String kbnCode		= this.getParameter("srhDlgTxtKbnCode");
+		String code			= this.getParameter("srhDlgTxtCode");
+		
+		//不使用
+		String kbnName		= null;
+		
+		
+		// DB接続
+		Connection con		= this.getConnection("kintai", req);
+		
+		//=====================================================================
+		// 結果返却
+		//=====================================================================
+		
+        this.addContent("result",  PJActionBase.getMstKubuns(con, kbnCode, code, kbnName));
+    	
+	}
+}
