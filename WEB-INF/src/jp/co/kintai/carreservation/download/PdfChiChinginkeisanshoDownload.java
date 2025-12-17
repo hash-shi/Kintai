67da5da196e9a2030e937f55686613602bc7fe2c
ishi-nat <ishi-nat@tjs-net.co.jp>
2025-12-17 17:39:35 +0900

    動作検証対応No.6 帳票側対応
    勤怠リストの賃金計算書出力にて、勤務時間・有給休暇日数の半休の加減算出を修正。

--- WEB-INF/src/jp/co/kintai/carreservation/download/PdfChiChinginkeisanshoDownload.java
+++ WEB-INF/src/jp/co/kintai/carreservation/download/PdfChiChinginkeisanshoDownload.java
@@ -185,11 +185,11 @@ public class PdfChiChinginkeisanshoDownload extends DownloadBase {
 		sql.append(" 	,SUBSTRING(CONVERT(TEXT, TokkiJiko), 133, 66) AS TokkiJiko3 ");
 		sql.append(" 	,SUBSTRING(CONVERT(TEXT, TokkiJiko), 198, 66) AS TokkiJiko4 ");
 		
-		sql.append(" 	,CAST(CAST(K.ShinseiNissu01 + K.ShinseiNissu06 AS DECIMAL(4,1)) AS VARCHAR) AS ShinseiNissu01 ");
+		sql.append(" 	,CAST(CAST(K.ShinseiNissu01 AS DECIMAL(4,1)) AS VARCHAR) AS ShinseiNissu01 ");
 		sql.append(" 	,CAST(0.0 AS VARCHAR) AS ShinseiNissu02 ");
 		sql.append(" 	,CAST(0.0 AS VARCHAR) AS ShinseiNissu03 ");
 		sql.append(" 	,CAST(CAST(K.ShinseiNissu04 AS DECIMAL(4,1)) AS VARCHAR) AS ShinseiNissu04 ");
-		sql.append(" 	,CAST(CAST(K.ShinseiNissu05 AS DECIMAL(4,1)) AS VARCHAR) AS ShinseiNissu05 ");
+		sql.append(" 	,CAST(CAST(K.ShinseiNissu05 + K.ShinseiNissu06 AS DECIMAL(4,1)) AS VARCHAR) AS ShinseiNissu05 ");
 		sql.append(" 	,CAST(CAST(K.ShinseiNissu06 AS DECIMAL(4,1)) AS VARCHAR) AS ShinseiNissu06 ");
 		sql.append(" 	,CAST(CAST(K.ShinseiNissu07 AS DECIMAL(4,1)) AS VARCHAR) AS ShinseiNissu07 ");
 		sql.append(" 	,CAST(CAST(K.ShinseiNissu08 AS DECIMAL(4,1)) AS VARCHAR) AS ShinseiNissu08 ");
